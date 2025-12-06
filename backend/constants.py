2ce0968c228f5ccc53cb79ab7db43d61aae80d12
AndresTrujillo1811-bit <andresgtrujillof@gmail.com>
2025-12-06 13:20:32 +0100

    ingesting md files

--- backend/constants.py
+++ backend/constants.py
@@ -2,6 +2,3 @@ from pathlib import Path
 
 DATA_PATH = Path(__file__).parents[1] / "data"
 VECTOR_DATABASE_PATH = Path(__file__).parents[1] / "knowledge_base"
-
-print(DATA_PATH)
-print(VECTOR_DATABASE_PATH)
--- backend/data_models.py
+++ backend/data_models.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, BaseModel
 from lancedb.embeddings import get_registry
 from lancedb.pydantic import Vector, LanceModel
 from dotenv import load_dotenv
@@ -7,7 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 embedding_model = get_registry().get("gemini-text").create(name="gemini-embedding-001")
 
+EMBEDDING_DIM = 3072
+
+
 class TranscriptYT(LanceModel): 
-    id: str
+    transcript_id: str = Field(description="ID of each transcript")
+    source: str = Field(description="Name of the md files")
     content: str = embedding_model.SourceField()
+    embedding: Vector(EMBEDDING_DIM) = embedding_model.VectorField()
     
+    
+class Prompt(BaseModel):
+    prompt: str = Field(description="Message or question from the user.")
+    
+    
+class RAGResponse(BaseModel):
+    answer: str = Field(description="Answer from the user.")
